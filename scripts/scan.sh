a2e8053c89a891f1d31148b7048f453c0a7be857
rpqq <yb206ray@gmail.com>
2025-04-03 10:54:51 +0100

    K: kredit kards

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,11 @@
-# TODO: write the script
+echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "========= SCANNING FOR SOCIAL SECURITY NUMBERS  ========="
+echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "========= SOCIAL SECURITY SCAN COMPLETE ========="
+echo "\n========= SCANNING FOR PHONE NUMBERS  ========="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "========= SCANNING FOR PHONE NUMBERS  ========="
+
